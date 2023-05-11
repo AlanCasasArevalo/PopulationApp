3ad787724e26cda1a1e6bdb0ffb1a72bc471221e
Alan Casas <alan.casas@edreamsodigeo.com>
2023-05-11 17:42:33 +0200

    Main model was added to change three services models with just one

--- PopulationAppTests/MainServiceAdapterTests.swift
+++ PopulationAppTests/MainServiceAdapterTests.swift
@@ -1,4 +1,11 @@
 import XCTest
+import PopulationApp
+
+struct MainModel {
+    let posts: [Post]
+    let users: [User]
+    let products: [Product]
+}
 
 final class MainServiceAdapterTests: XCTestCase {
     func test() {

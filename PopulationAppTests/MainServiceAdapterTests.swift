26bd9ffe4d12abd0e0635b7d44e4f258aa9f39c5
Alan Casas <alan.casas@edreamsodigeo.com>
2023-05-12 11:24:13 +0200

    Refactor code to be improved

--- PopulationAppTests/MainServiceAdapterTests.swift
+++ PopulationAppTests/MainServiceAdapterTests.swift
@@ -24,27 +24,68 @@ class MainLoaderAdapter: MainLoader {
         self.productsLoader = productsLoader
     }
     
-    func load(completion: @escaping (MainLoader.Result) -> ()) {
-        postsLoader.load { posts in
-            if case let .failure(error) = posts {
-                return completion(.failure(error))
-            }
-            self.usersLoader.load { users in
-                if case let .failure(error) = users {
-                    return completion(.failure(error))
-                }
-                
-                self.productsLoader.load { products in
-                    if case let .failure(error) = products {
-                        return completion(.failure(error))
-                    }
-                    
-                    completion(.success(MainModel(
-                        posts: try! posts.get(),
-                        users: try! users.get(),
-                        products: try! products.get()))
+    struct PartialResult {
+        var posts: [Post]?{
+            didSet { checkCompletion() }
+        }
+        
+        var users: [User]?{
+            didSet { checkCompletion() }
+        }
+        
+        var products: [Product]?{
+            didSet { checkCompletion() }
+        }
+
+        var error: Error?{
+            didSet { checkCompletion() }
+        }
+        
+        var completion: ((MainLoader.Result) -> ())?
+        
+        private mutating func checkCompletion() {
+            if let error = error {
+                completion?(.failure(error))
+                completion = nil
+            } else if let posts = posts, let users = users, let products = products {
+                completion?(
+                    .success(
+                        MainModel(posts: posts, users: users, products: products)
                     )
-                }
+                )
+                completion = nil
+            }
+        }
+    }
+    
+    func load(completion: @escaping (MainLoader.Result) -> ()) {
+        
+        var partialResult = PartialResult(completion: completion)
+        
+        postsLoader.load { result in
+            switch result {
+            case .success(let value):
+                partialResult.posts = value
+            case .failure(let error):
+                partialResult.error = error
+            }
+        }
+        
+        usersLoader.load { result in
+            switch result {
+            case .success(let value):
+                partialResult.users = value
+            case .failure(let error):
+                partialResult.error = error
+            }
+        }
+        
+        productsLoader.load { result in
+            switch result {
+            case .success(let value):
+                partialResult.products = value
+            case .failure(let error):
+                partialResult.error = error
             }
         }
     }
